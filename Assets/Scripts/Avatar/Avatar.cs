c68847179f1f19db0f069c34fd52f8aba5ca7b34
Aylin Acikel <aylincacikel@gmail.com>
2021-05-04 17:32:17 +0200

    Building additive scene load structure
    Adding base scene to logic scene and helper and palyer to base scene with no player controller

--- Assets/Scripts/Avatar/Avatar.cs
+++ Assets/Scripts/Avatar/Avatar.cs
@@ -14,14 +14,35 @@ public class Avatar : MonoBehaviour
     public float lerpDistanceFarSummand = 40f;
     //used for near stop distance
     public float lerpDuration = 35f;
+    public float scalingFactor = 1f;
+    private Vector3 avatarLocalScale = Vector3.one;
+    private CapsuleCollider2D capsuleTrigger;
+    private Rigidbody2D rigidbody2d;
 
     private void Start()
     {
-
+        avatarLocalScale = this.gameObject.transform.localScale;
+        capsuleTrigger = this.gameObject.GetComponent<CapsuleCollider2D>();
+        rigidbody2d = this.gameObject.GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
 
+    }
+
+    public CapsuleCollider2D getCapsuleTrigger()
+    {
+        return capsuleTrigger;
+    }
+
+    public Rigidbody2D getRigidbody2D()
+    {
+        return rigidbody2d;
+    }
+
+    public Vector3 getLocalScale()
+    {
+        return avatarLocalScale;
     }
 }
