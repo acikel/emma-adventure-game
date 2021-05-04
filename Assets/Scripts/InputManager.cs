899bbb2ee8282607fe423b6cf6f7974fa4e96538
Aylin Acikel <aylincacikel@gmail.com>
2021-05-04 17:54:43 +0200

    Adding Input Manager To logic

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private Vector3 mousePosition;
+    private Vector2 mousePositionWorld2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
