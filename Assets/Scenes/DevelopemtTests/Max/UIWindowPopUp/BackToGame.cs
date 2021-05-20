1147cefed06861796bd99df8db7214febd15f7f3
Agata <agata.tselesh@zhdk.ch>
2021-05-20 16:32:32 +0200

    PupUpPanes_Fix

--- Assets/Scenes/DevelopemtTests/Max/UIWindowPopUp/BackToGame.cs
+++ Assets/Scenes/DevelopemtTests/Max/UIWindowPopUp/BackToGame.cs
@@ -5,15 +5,23 @@ using UnityEngine.EventSystems;
 
 public class BackToGame : MonoBehaviour, IPointerClickHandler
 {
-    public GameObject Window;
+    public GameObject Panel;
 
 
 
     public void OnPointerClick(PointerEventData pointerEventData)
     {
-        if (Window.activeSelf == true)
-        {
-            Window.SetActive(false);
+        
+            
+        if (Panel.activeSelf == true){
+            Panel.SetActive(false);
+            //Debug.Log("Window not here");
+        } else{
+            Panel.SetActive(true);
+           // Debug.Log("Window here");
         }
+        
+        
+
     }
 }
