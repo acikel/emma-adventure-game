5dcfa8ccd77406d0b591c988c5b71308b7ee1f9a
Aylin Acikel <aylincacikel@gmail.com>
2021-05-24 00:46:03 +0200

    Lock settings adjustment
    And Scripts moved.

--- Assets/Scenes/DevelopemtTests/Aylin/Scripts/Lock/LockResumePanel.cs
+++ Assets/Scenes/DevelopemtTests/Aylin/Scripts/Lock/LockResumePanel.cs
@@ -52,7 +52,7 @@ public class LockResumePanel : MonoBehaviour, IPointerClickHandler
 
     private IEnumerator waitAndCloseLock()
     {
-        yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(0.2f);
         openCloseLockInventroyCanvas(false);
     }
     public void openLockCanvas()
