44043f5d689dd2d1852a9246effc905f93e4da53
Aylin Acikel <aylincacikel@gmail.com>
2021-05-28 14:56:12 +0200

    Disabled helper control

--- Assets/Scenes/DevelopemtTests/Aylin/Scripts/Logic/PlayerControler.cs
+++ Assets/Scenes/DevelopemtTests/Aylin/Scripts/Logic/PlayerControler.cs
@@ -162,7 +162,7 @@ public class PlayerControler : MonoBehaviour
             }
             //else if (inputManager.getRaycastMainHitOnMouseDown().rigidbody != null && inputManager.getRaycastMainHitOnMouseDown().rigidbody.tag == "Helper")
 
-            //TODO UNCOMMENT TO GET CONTROL OVER HELPER BY CLICKING ON HIM
+            /*//TODO UNCOMMENT TO GET CONTROL OVER HELPER BY CLICKING ON HIM
             else if ((raycastHit = inputManager.getRaycastRigidbody("Helper")).rigidbody != null)
             {
                 if(raycastHit.rigidbody.gameObject != AvatarManager.currentAvatar.gameObject && !isMoving)
@@ -171,7 +171,7 @@ public class PlayerControler : MonoBehaviour
                     colliderOfAvatarCurrent = colliderOfAvatarHelper;
                 }
             }
-            
+            */
             else if (inputManager.checkIfColliderWasHit("SelectedItem"))
             {
 
