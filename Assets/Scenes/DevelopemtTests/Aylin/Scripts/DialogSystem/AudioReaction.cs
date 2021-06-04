71b6671c522d63751bc7bc66d9487fae6cde5112
Aylin Acikel <aylincacikel@gmail.com>
2021-06-04 18:26:48 +0200

    Dialog system working first sound bug fixed

--- Assets/Scenes/DevelopemtTests/Aylin/Scripts/DialogSystem/AudioReaction.cs
+++ Assets/Scenes/DevelopemtTests/Aylin/Scripts/DialogSystem/AudioReaction.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 // This Reaction is used to play sounds through a given AudioSource.
@@ -31,6 +32,7 @@ public class AudioReaction : Reaction
     [FMODUnity.EventRef]
     public string textAudio; // Reference to the AudioClip to be played.
     private FMOD.Studio.EventInstance audioEvent;
+    //private FMOD.Studio.EventInstance audioEvent2;
     private FMOD.Studio.EventDescription eventDescription;
 
     private void OnEnable()
@@ -40,6 +42,7 @@ public class AudioReaction : Reaction
 
         eventDescription = FMODUnity.RuntimeManager.GetEventDescription(textAudio);
         eventDescription.createInstance(out audioEvent);
+
         TextManager.OnEndTypeWriting += stopCurrentSound;
     }
     private void OnDisable()
@@ -53,17 +56,25 @@ public class AudioReaction : Reaction
 
     protected override void ImmediateReaction()
     {
-        // Set the AudioSource's clip to the given one and play with the given delay.
+        //Debug.Log("start sound1: "+ textAudio);
+
         if (textAudio.Length!=0)
         {
-            //FMODUnity.RuntimeManager.PlayOneShot(textAudio);
+
+            //This is only not working for the first loaded voice in the dream scene. If one should be played with the first reaction
+            //Thats way the first audio reaction need to be in the scene as seperate objecgt with a fmod emiiter event and an collider
+            //which is not on the player (to not collide with it) and above all colliders (z=100) and play on start (stop on mouse down)
+            //This way the first sound gets activated right away and stops when the text is stopped.
+            //To see the object look at SoundToPlayOnLoad game object of scene Sequence1DialogSystem which is in the folder Assets/Scenes/DevelopmentTests/Aylin/DialogSystem
+            //FMODUnity.RuntimeManager.PlayOneShot(textAudio); would work for the first sound but then it cant be stopped when wanting to skip the text.
             audioEvent.start();
         }
     }
 
+
     private void stopSound(FMOD.Studio.EventInstance instance)
     {
-        Debug.Log("stop sound");
+        //Debug.Log("stop sound");
         instance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
     }
 
